7faac8d9b1bb62ab6b45cb149dd353dbfffcb294
amojena <am3238@cornell.edu>
2020-03-26 22:28:29 -0400

    yay

---
+++ rehab/Assets/_Stroke/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // Initalize variables
+    public GameObject balloonPrefab;
+
+    // Initialize private variables
+    private List<GameObject> balloons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balloons = new List<GameObject>();
+        balloons.Add(Instantiate(balloonPrefab, new Vector3(1, 0.5f, 0), Quaternion.identity));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
