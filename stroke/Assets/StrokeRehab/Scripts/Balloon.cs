ffbdfcf848d108ba541ec7f50edd61f5e0e1c03c
amojena <am3238@cornell.edu>
2020-03-26 20:13:33 -0400

    Hoping for the best

--- stroke/Assets/StrokeRehab/Scripts/Balloon.cs
+++ stroke/Assets/StrokeRehab/Scripts/Balloon.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
+    private static int numBalloons = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.Debug.Log("Hello");
+        SphereCollider sphere = gameObject.GetComponentInChildren<SphereCollider>();
+        sphere.center = new Vector3(-.67f, 0.87f, -0.1f);
+        sphere.radius = 0.24f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    void OnCollisionEnter(Collision collision)
-    {
-        gameObject.SetActive(false);
-        UnityEngine.Debug.Log("Yooooooooo");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        gameObject.SetActive(false);
-        UnityEngine.Debug.Log("Triggered");
+
+        Debug.LogFormat("%d", numBalloons);
+        if (++numBalloons % 2 == 0)
+            gameObject.transform.position -= new Vector3(1, 0, 0);
+
+        else
+            gameObject.transform.position += new Vector3(1, 0, 0);
+
+        if (numBalloons == 6)
+        {
+            //gameObject.SetActive(false);
+            gameObject.transform.position += new Vector3(0, 0, 0.6f);
+        }
     }
 }
