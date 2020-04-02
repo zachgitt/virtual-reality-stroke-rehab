43c13e8b4974c981f998d580d4d0e577fd4cd07b
amojena <am3238@cornell.edu>
2020-04-01 22:44:56 -0400

    Grabbing works

---
+++ rehab/Assets/_Stroke/Scripts/HandGrabbing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGrabbing : OVRGrabber
+{
+    private OVRHand hand;
+    private float pinchThreshold = 0.7f;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        hand = GetComponent<OVRHand>();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+        CheckPinchIndex();      
+    }
+
+    void CheckPinchIndex()
+    {
+        float pinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        bool isGrabbing = !m_grabbedObj && (pinchStrength > pinchThreshold)
+            && m_grabCandidates.Count > 0;
+        bool isReleasing = m_grabbedObj && !(pinchStrength > pinchThreshold);
+
+        if (isGrabbing)
+            GrabBegin();
+        else if (isReleasing)
+            GrabEnd();
+    }
+}
