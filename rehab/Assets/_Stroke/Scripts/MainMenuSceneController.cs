0b318100ab679df0b9c9c427119512a9ce80bc35
Zachary Gittelman <zachgitt@umich.edu>
2020-04-06 01:50:07 -0400

    added main menu

---
+++ rehab/Assets/_Stroke/Scripts/MainMenuSceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSceneController : MonoBehaviour
+{
+    public Scene balloonScene;
+    public Scene basketScene;
+    public Scene cubeScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SceneManager.LoadScene(other.scene))
+        {
+            Debug.Log("TODO");
+        }
+    }
+}
