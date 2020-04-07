3a5fad285bcbd054460b7ed4d4fd2bb94561f5a4
Zachary Gittelman <zachgitt@umich.edu>
2020-04-06 21:32:42 -0400

    completed the main menu and doublePinchForMenu!

---
+++ rehab/Assets/_Stroke/Scripts/DoublePinchForMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static OVRHand;
+using UnityEngine.SceneManagement;
+
+
+public class DoublePinchForMenu : MonoBehaviour
+{
+    public OVRHand leftHand;
+    public OVRHand rightHand;
+    public float threshold;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftPinchStrength = leftHand.GetFingerPinchStrength(HandFinger.Index);
+        float rightPinchStrength = rightHand.GetFingerPinchStrength(HandFinger.Index);
+        if (leftPinchStrength > threshold && rightPinchStrength > threshold)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ rehab/Assets/_Stroke/Scripts/LoadSceneScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadSceneScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (this.gameObject.tag)
+        {
+            case "BalloonScene":
+                SceneManager.LoadScene(1);
+                break;
+
+            case "BasketScene":
+                SceneManager.LoadScene(2);
+                break;
+
+            case "CubeScene":
+                SceneManager.LoadScene(3);
+                break;
+
+            default:
+                Debug.Log("This should not be reached.");
+                break;
+        }
+    }
+}
