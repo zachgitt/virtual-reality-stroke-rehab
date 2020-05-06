7dfd2194bd4e75963a132ab62bdf3ddfd50c0daa
amojena <am3238@cornell.edu>
2020-05-06 18:48:24 -0400

    Working on getting the prefabs for veggies and meat, game logic after

---
+++ rehab/Assets/_Stroke/Scripts/CashierSceneController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashierSceneController : MonoBehaviour
+{
+    public GameObject belt;
+    public GameObject fruitBox;
+    public GameObject meatBox;
+    public GameObject dairyBox;
+
+    private List<Vector3> startPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomizeBagOrder();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RandomizeBagOrder()
+    {
+        startPos = new List<Vector3>()
+        {
+            fruitBox.transform.position,
+            meatBox.transform.position,
+            dairyBox.transform.position
+        };
+
+        for (int i = 0; i < startPos.Count; i++)
+        {
+            Vector3 temp = startPos[i];
+            int swap = Random.Range(i, startPos.Count);
+            startPos[i] = startPos[swap];
+            startPos[swap] = temp;
+        }
+
+        fruitBox.transform.position = startPos[0];
+        meatBox.transform.position = startPos[1];
+        dairyBox.transform.position = startPos[2];
+    }
+}
